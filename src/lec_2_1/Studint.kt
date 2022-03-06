94e232399e66c9eb16ec347c7911381e9ce82978
abad-I-go7sh <100103711+abad-I-go7sh@users.noreply.github.com>
2022-03-07 00:41:52 +0200

    Add files via upload

---
+++ src/lec_2_1/Studint.kt
@@ -0,0 +1,9 @@
+package lec_2_1
+
+class Studint(var id: Int, var name: String, var email: String, var password: String, var gba: Double) {
+    //عبارة عن كونستركتر امعرفها دغري
+    c
+}
+
+
+
---
+++ src/lec_2_1/main.kt
@@ -0,0 +1,7 @@
+package lec_2_1
+
+fun main() {
+    var s1 = Studint(10, "abed", "abed@", "123456", 93.2)
+    s1.id
+
+}
